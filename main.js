2b0def162c90528cc26f29475950a6a26b9a94a5
LuisLCalvi <calvilautaro5@gmail.com>
2022-04-19 22:19:25 -0300

    se agrego funciones

--- main.js
+++ main.js
@@ -1,47 +1,114 @@
+//Algoritmo Simple
+alert("Bienvenido, buena jornada laboral")
+
+let nombre = prompt ("Ingrese su nombre ");
+
+let apellido = prompt ("Ingrese su apellido");
+
+let resultado = nombre + " " + apellido;
+
+//Algoritmo con condicional
+if (nombre == "") { alert("No ingresaste tu nombre")};
+
+if (apellido == "") { alert("No ingresaste tu apellido")};
+
+
+if ((nombre !="") && (apellido !="")){
+    alert ("Bienvenido" + " " + resultado + " " + "listo para comenzar a gestionar turnos")
+    };
 
     //funcion
 
-let paciente = nombre + " " + apellido;
-
-solicitarTurno ();
-
-function solicitarTurno () {
-    let turno = confirm ("pedir turno")
-    solicitarDatos (datos)
-}
+solicitarDatos ();
 
 function solicitarDatos (d) {
-    if (d) {
-        let nombre = prompt ("Ingresar Nombre completo");
-        let apellido = prompt ("Ingresar Apellido")
-        especialidadMedica (medico)
+    prompt ("Ingresar Nombre completo y Apellido del paciente");
 
-    } else{
-        alert("Es requerimiento completar los datos solicitados")
+
     }
 
-}
+seleccionarMedico ()
 
-function especialidadMedica () {
+
+function seleccionarMedico () {
     console.log ("1: traumatologia")
     console.log ("2: endocrinologia")
-    console.log ("3. clinica")
-    console.log ("4: neumonologia")
-    console.log ("5: neurologia")
-    console.log ("6. radiologia")
+    console.log ("3. radiologia")
+    console.log ("4: neurologia")
+    console.log ("5: clinica")
+    console.log ("6. gastroenterologia")
+    console.log ("7: cirugia")
+    console.log ("8: cardiologia")
+    console.log ("9. oftalmologia")
+    console.log ("10: psiquiatria")
+    console.log ("--------")
+
 }
+
 
 let op = prompt("Ingresar opcion")
 
-switch (op){
-    case "1":     for (let i = 1; i <= 10; i++) {
-        console.log (" Turno N° "+i+ paciente);
 
-    };
+switch (op){     
+    case "1":     
+    alert ("Se guardo turno")
+
+
     break;
 
+    case "2":
+    alert ("Se guardo turno")
 
-}
+    break;
+
+    case "3":
+    alert ("Se guardo turno")
+
+    break;
+
+    case "4":
+        alert ("Se guardo turno")
+
+        break;
+    
+    case "5":
+        alert ("Se guardo turno")
+
+        break;
+
+    case "6":
+        alert ("Se guardo turno")
+
+        break;
+
+    case "7":
+        alert ("Se guardo turno")
+
+        break;
+
+    case "8":
+        alert ("Se guardo turno")
+
+        break;
+
+    case "9":
+        alert ("Se guardo turno")
+
+        break;
+    
+    case "10":
+        alert ("Se guardo turno")
+
+        break;
+
+    default:
+        console.log("Operacion Invalida")
+        break;
+};
+
+
+
+
 
 
 
