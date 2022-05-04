8d387717754564339d5fa861a3c627e3ab7417f3
LuisLCalvi <calvilautaro5@gmail.com>
2022-05-04 20:30:42 -0300

    dom

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
+//DOM
+
+
+const encabezado = document.getElementById("titulop")
+console.log(titulop)
+console.log(encabezado.innerHTML)
 
 
 
 
+encabezado.innerText = " Se modifico el nombre"
 
+encabezado.innerHTML = "<strong> Nuevamente modificamos el nombre </strong>"
 
+encabezado.style.color = "blue"
 
+const menu = document.getElementById("navegacion")
 
+console.log(menu)
 
+const link = document.createElement ('a')
 
+link.textContent = "Nueva opcion"
 
+link.href = (/nuevo/enlace.html)
 
+link.classList.add ('link')
 
-
-
-
-
-
-
+menu.appendChild (link)
