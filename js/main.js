be068a64379e265cd1b2121497e78175c1b04a5a
LuisLCalvi <calvilautaro5@gmail.com>
2022-05-18 17:57:35 -0300

    agregando operaciones y realizando modificaciones

--- js/main.js
+++ js/main.js
@@ -1,19 +1,58 @@
 //Algoritmo Simple
-const nombre = document.querySelector ('#nombreu')
-const apellido = document.querySelector ('#apellidou')
-const nombreApellido = nombre + " " + apellido;
-const btnRegistro = document.querySelector ("#btn")
-const btningresaru = document.querySelector ('#btningresaru')
+
 
 //Algoritmo con condicional
 //const error = alert ("No se cargaron de manera correcta los datos, por favor ingrese los datos solicitados");
-if (nombre === "") { alert ("error")};
 
-if (apellido === "") { alert("error")};
-
+const formulario = document.querySelector ('#formulario')
 
 
-btnRegistro.addEventListener("click", ()=>{
-console.log ("Se registro con exito")
-})
+formulario.addEventListener ("submit", agregarRegistro)
 
+
+function agregarRegistro (evt) {
+    evt.preventDefault();
+    const nombreU = document.querySelector ('#ingresarN').value
+    const apellidoU = document.querySelector ('#ingresarA').value
+    let usuario = nombreU + " " + apellidoU
+
+        //OPERACIONES AVANZADAS 
+        nombreU === "" && mostrarError ("No se cargaron de manera correcta los datos, por favor ingrese los datos solicitados")
+
+        apellidoU === "" && mostrarError ("No se cargaron de manera correcta los datos, por favor ingrese los datos solicitados")
+
+        nombreU !="", apellidoU !="" && mostrarMensaje ("Bienvenido" + " " + usuario + " " + "se ha registrado con éxito, ya puede ingresar")        
+
+        function mostrarError (error) {
+            const mensajeError = document.createElement ('p')
+            mensajeError.textContent = error;
+            mensajeError.classList.add ('error')
+
+            const contenido = document.querySelector('.contenido')
+            contenido. appendChild (mensajeError)
+
+            setTimeout (()=> {
+                mensajeError.remove ()
+            }, 2000 )
+
+        }
+
+        function mostrarMensaje (inicio) {
+            const mensajeInicio = document.createElement ('p')
+            mensajeInicio.textContent = inicio;
+            mensajeInicio.classList.add ('inicio')
+            const contenido = document.querySelector('.contenido')
+            contenido. appendChild (mensajeInicio)
+
+            setTimeout (()=> {
+                mensajeInicio.remove ()
+            }, 3000 )
+
+        }
+        localStorage.setItem (nombreU, apellidoU)
+
+
+
+
+
+}
--- js/principal.js
+++ js/principal.js
@@ -97,221 +97,154 @@ writeMonth(mesNumero);
 
 //Gestion de Turnos
 
+const especialidades = ["traumatologia", "neurologia", "endocrinologia", "clinica", "ginecologia", "urologia", "psiquiatria", "gastroenterologia"]
 
-//OBJETO
-let nombrePaciente = prompt ("Ingrese Nombre del paciente");
-while (nombrePaciente != "salir"){
-let apellidoPaciente = prompt ("Ingrese Apellido del paciente");
-let obraSocial = prompt ("Ingrese Obra Social del paciente");
+const traumatologia = {
+    id: 1,
+    medico: "Lombardo Mauro",
+    dias: "lunes - miercoles",
 
-
-
-
-    const paciente = { 
-        nombrePaciente: (nombrePaciente),
-        apellidoPaciente: (apellidoPaciente),
-        obraSocial: (obraSocial),
-
-    }
-    
-
-
-    console.log(paciente)
-
-    alert(paciente.nombrePaciente + " " + paciente.apellidoPaciente + " " + paciente.obraSocial)
-
-
-
-seleccionarEspecialidad ()
-
-
-function seleccionarEspecialidad () {
-    console.log ("1: traumatologia")
-    console.log ("2: endocrinologia")
-    console.log ("3. ginecologia")
-    console.log ("4: neurologia")
-    console.log ("5: clinica")
-    console.log ("6. gastroenterologia")
-    console.log ("7: cirugia")
-    console.log ("8: cardiologia")
-    console.log ("9. oftalmologia")
-    console.log ("10: psiquiatria")
-    console.log ("--------")
+    id: 2,
+    medico: "Almeida Julio",
+    dias: "martes - jueves",
 
 }
 
-const traumatologia = [
-    {medico: 'Dr. Lombardo Mauro', dias: ' lunes - miercoles - viernes'},
-    {medico:'Dr. Almeida Julio',  dias: 'lunes - martes - miercoles - viernes'},]
-const endocrinologia = [
-    {medico: 'Dra. Ramirez, Mercedes', dias: 'martes - miercoles - jueves'}, 
-    {medico: 'Dra. Gadea Monica', dias: 'lunes - viernes'},]
-const ginecologia = [
-    {medico: 'Dra. Altamirano Liz', dias: 'martes - jueves'}, 
-    {medico: 'Dr. Sirimarco, Federico', dias: 'lunes - viernes'},]
-const neurologia = [
-    {medico: 'Dr. Falco Nicolas', dias: 'miercoles'},
-    {medico: 'Dr. Euler Javier', dias: 'lunes - viernes'},]
-const clinica = [
-    {medico: 'Dr. Mones Ruiz Matias', dias: 'lunes - martes - miercoles - jueves'},
-    {medico: 'Dra. Ramirez Yamila', dias: 'martes - viernes'},]
-const gastroenterologia = [
-    {medico: 'Garcia Pinasco Oscar', dias: 'lunes - miercoles'},
-    {medico: 'Notari Lorena', dias: 'martes - jueves'}, ]
-const cirugia = [
-    {medico: 'Parra Eduardo', dias: 'lunes - jueves - viernes'},
-    {medico: 'Castro Ivana', dias: 'martes - miercoles'},]
-const cardiologia = [
-    {medico: 'Gadea Jose', dias: 'lunes - miercoles - jueves'},
-    {medico: 'Navaja Ricardo', dias: 'martes - viernes'},]
-const oftalmologia = [
-    {medico: 'Grecco Diego', dias: 'martes - viernes'},
-    {medico: 'Navarro Guillermo', dias: 'lunes - miercoles - jueves'},]
-const psiquiatria = [
-    {medico: ' Gianello Federico', dias: 'lunes - martes - miercoles - jueves'},
-    {medico: ' Peretti Diego', dias: 'viernes'},]
+const neurologia = {
+    id: 1,
+    medico: "Falco Nicolas",
+    dias: "lunes - miercoles - jueves",
+
+    id: 2,
+    medico: "Euler Raul",
+    dias: "martes - viernes",
+}
+
+const endocrinologia = {
+    id: 1,
+    medico: "Ramirez Maria de las Mercedes",
+    dias: "lunes - martes - jueves - viernes",
+
+    id:2,
+    medico: "Gadea Florencia",
+    dias: "martes - miercoles - viernes",
+
+}
+
+const clinica = {
+    id: 1,
+    medico: "Mones Ruiz Matias",
+    dias: "lunes - jueves",
+
+    id:2,
+    medico: "Ramirez yamila",
+    dias: "martes - miercoles - viernes"
+}
+
+const ginecologia = {
+    id:1,
+    medico: "Altamirano, Liz",
+    dias: "lunes - miercoles - jueves",
+
+    id: 2,
+    medico: "Sirimarco Federico",
+    dias: "lunes - martes - viernes",
+
+}
+
+const urologia = {
+    id: 1,
+    medico: "Barbara Francisco",
+    dias: "lunes - martes - miercoles - jueves - viernes",
+
+    id: 2,
+    medico: "Rapallo, Martin",
+    dias: "martes - jueves",
+}
+
+const psiquiatria = {
+    id: 1,
+    medico: "Gianello Raul",
+    dias: "lunes - miercoles - jueves - viernes",
+
+    id: 2,
+    medico: "Peretti Diego",
+    dias: "martes - miercoles - viernes",
+}
+
+const gastroenterologia = {
+    id:1,
+    medico: "Garcia Pinasco Oscar",
+    dias: "lunes - martes - miercoles - jueves - viernes",
+
+    id: 2,
+    medico: "Notari Lorena",
+    dias: "lunes - miercoles - viernes",
+}
+
+const formularioP = document.querySelector ('#formularioP')
+
+formularioP.addEventListener ("submit", agregarPaciente)
 
 
+function agregarPaciente (evt){
+    evt.preventDefault();
+    const nombrePaciente = document.querySelector ('#nombreP').value
+    const apellidoPaciente = document.querySelector('#apellidoP').value
+    const obraSocial = document.querySelector ('#obrasocialP').value
+    const dniPaciente = document.querySelector ('#dniP').value
+    const telefonoPaciente = document.querySelector ('#telP').value
+    const paciente = nombrePaciente + " " + apellidoPaciente + " "+ dniPaciente
 
 
+    // DESTRUCTURACION
 
-    let op = prompt("Seleccionar especialidad (numero)")
-
-
-    switch (op){     
-        case "1":
-                console.log (traumatologia [0])
-                console.log (traumatologia [1])
-                let  = prompt ("Seleccionar Medico"),
-                alert("Estimado " + " " + paciente.nombrePaciente  + " " + paciente.apellidoPaciente + " " + "Se guardo su turno")
-
-                //LOCAL STORAGE
-                let turno = localStorage.setItem (paciente.nombrePaciente, paciente.apellidoPaciente)
-
-                //JSON
-                const enJSON = JSON.stringify (traumatologia);
-                
-                console.log (enJSON)
-
-            
-
-                nombrePaciente = prompt ("Ingrese nombre del paciente")
-    
-        break;
-    
-        case "2":
-            console.log (endocrinologia [0])
-            console.log (endocrinologia [1])
-            let  = prompt ("Seleccionar Medico"),
-    
-            alert("Estimado " + " " + paciente.nombrePaciente  + " " + paciente.apellidoPaciente + " " + "Se guardo su turno" )
-
-               //SESSION STORAGE
-            
-            let cliente = sessionStorage.setItem (paciente.nombrePaciente, paciente.obraSocial)
-
-            
-            nombrePaciente = prompt ("Ingrese nombre del paciente")
-    
-        break;
-    
-        case "3": 
-            console.log (ginecologia [0])
-            console.log (ginecologia [1])
-            let  = prompt ("Seleccionar Medico"),
-            alert("Estimado " + " " + paciente.nombrePaciente  + " " + paciente.apellidoPaciente + " " + "Se guardo su turno" )
-
-            nombrePaciente = prompt ("Ingrese nombre del paciente")
-    
-        break;
-    
-        case "4":
-            console.log (neurologia [0])
-            console.log (neurologia [1])
-            let  = prompt ("Seleccionar Medico"),
-            alert("Estimado " + " " + paciente.nombrePaciente  + " " + paciente.apellidoPaciente + " " + "Se guardo su turno")
-
-            nombrePaciente = prompt ("Ingrese nombre del paciente")
-    
-    
-            break;
-        
-        case "5":
-            console.log (clinica [0])
-            console.log (clinica [1])
-            let  = prompt ("Seleccionar Medico"),
-            alert("Estimado " + " " + paciente.nombrePaciente  + " " + paciente.apellidoPaciente + " " + "Se guardo su turno")
-
-            nombrePaciente = prompt ("Ingrese nombre del paciente")
-    
-    
-            break;
-    
-        case "6":
-            console.log (gastroenterologia [0])
-            console.log (gastroenterologia [1])
-            let  = prompt ("Seleccionar Medico"),
-            alert("Estimado " + " " + paciente.nombrePaciente  + " " + paciente.apellidoPaciente + " " + "Se guardo su turno")
-
-            nombrePaciente = prompt ("Ingrese nombre del paciente")
-    
-    
-            break;
-    
-        case "7":
-            console.log (cirugia [0])
-            console.log (cirugia [1])
-            let  = prompt ("Seleccionar Medico"),
-            alert("Estimado " + " " + paciente.nombrePaciente  + " " + paciente.apellidoPaciente + " " + "Se guardo su turno")
-
-            nombrePaciente = prompt ("Ingrese nombre del paciente")
-    
-    
-            break;
-    
-        case "8":
-            console.log (cardiologia [0])
-            console.log (cardiologia [1])
-            let  = prompt ("Seleccionar Medico"),
-            alert("Estimado " + " " + paciente.nombrePaciente  + " " + paciente.apellidoPaciente + " " + "Se guardo su turno")
-
-            nombrePaciente = prompt ("Ingrese nombre del paciente")
-    
-    
-            break;
-    
-        case "9":
-            console.log (oftalmologia [0])
-            console.log (oftalmologia [1])
-            let  = prompt ("Seleccionar Medico"),
-            alert("Estimado " + " " + paciente.nombrePaciente  + " " + paciente.apellidoPaciente + " " + "Se guardo su turno")
-
-            nombrePaciente = prompt ("Ingrese nombre del paciente")
-    
-    
-            break;
-        
-        case "10":
-            console.log (psiquiatria [0])
-            console.log (psiquiatria [1])
-            let  = prompt ("Seleccionar Medico"),
-            alert("Estimado " + " " + paciente.nombrePaciente  + " " + paciente.apellidoPaciente + " " + "Se guardo su turno")
-
-            nombrePaciente = prompt ("Ingrese nombre del paciente")
-    
-    
-            break;
-    
-        default:
-            console.log("Su turno es Invalido")
-            break;
-    
-    
-    };
-    
-    
-    
-    };
-    
+    let destructurar = (paciente) => {
+        const { nombrePaciente, apellidoPaciente} = paciente
+    }
+    console.log (nombrePaciente, apellidoPaciente)
 
     
+    nombrePaciente === "" && mostrarError ("No se cargaron de manera correcta los datos, por favor ingrese los datos solicitados")
+
+    apellidoPaciente === "" && mostrarError ("No se cargaron de manera correcta los datos, por favor ingrese los datos solicitados")
+
+    dniPaciente === "" && mostrarError ("No se cargaron de manera correcta los datos, por favor ingrese los datos solicitados")
+
+    nombrePaciente !="", apellidoPaciente !="", obraSocial != "", dniPaciente != "", telefonoPaciente !="" && mostrarMensaje ("Estimado" + " "+  paciente + " " + "se ha guardado su turno") 
+
+
+    function mostrarError (error) {
+        const mensajeError = document.createElement ('p')
+        mensajeError.textContent = error;
+        mensajeError.classList.add ('error2')
+
+        const turnera = document.querySelector('.turnera')
+        turnera. appendChild (mensajeError)
+
+        setTimeout (()=> {
+            mensajeError.remove ()
+        }, 2000 )
+
+    }
+
+    function mostrarMensaje (inicio) {
+            const mensajeInicio = document.createElement ('p')
+            mensajeInicio.textContent = inicio;
+            mensajeInicio.classList.add ('inicio2')
+            const turnera = document.querySelector('.turnera')
+            turnera. appendChild (mensajeInicio)
+
+            setTimeout (()=> {
+                mensajeInicio.remove ()
+            }, 3000 )
+
+}
+
+    function mostrarEspecialidades (especialidades){
+        const mostrarEspecialidad = document.createComment ('option')
+        mostrarEspecialidad.textContent = especialidades;
+        mostrarEspecialidad.classList.add ('mostrar__especialidad')
+        formularioP.appendChild (mostrarEspecialidad)
+    }
+}
